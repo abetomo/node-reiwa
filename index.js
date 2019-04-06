6b7a8e7384414b2eb8c6a24a610fb6dfa50696a0
abetomo <abe@enzou.tokyo>
2019-04-06 10:21:45 +0900

    Improve calculation logic

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-'use strict';
+'use strict'
+
+const REIWA_START_YEAR = 2019
+const REIWA_START_MONTH = 5
 
 module.exports.reiwa = function (date) {
   if (date == null) return null
-  return (date.getFullYear() - 2018)
+
+  const year = date.getFullYear()
+  if (year < REIWA_START_YEAR) return null
+  if (
+    year === REIWA_START_YEAR &&
+    date.getMonth() < (REIWA_START_MONTH - 1)
+  ) {
+    return null
+  }
+  return (year - REIWA_START_YEAR + 1)
 }
