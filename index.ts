2e14e17ecef45929ee9174770acdcb7def0be03c
abetomo <abe@enzou.tokyo>
2023-01-22 10:08:33 +0900

    feat: change to `export = reiwa`

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const REIWA_START_YEAR = 2019
 const REIWA_START_MONTH = 5
 
-export default function (date: Date): number | null {
+function reiwa (date: Date): number | null {
   if (date == null) return null
 
   const year = date.getFullYear()
@@ -14,3 +14,5 @@ export default function (date: Date): number | null {
   }
   return year - REIWA_START_YEAR + 1
 }
+
+export = reiwa
